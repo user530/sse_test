750c7eb09eeeaa6b0e87255f2b407cc33797ba3a
user530 <m.timarsuev@gmail.com>
2024-05-04 23:45:42 +0300

    Simple SSE endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable, interval, map } from 'rxjs';
 
-@Controller()
+interface IMessageEvent {
+  data: string | object;
+}
+
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('event')
+  sendEvent(): Observable<IMessageEvent> {
+    return interval(1000).pipe(
+      map(
+        (num: number) => ({ data: 'Hello ' + num })
+      )
+    )
   }
 }
